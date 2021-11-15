9dc18ba9ed3cc73ddf0ccbaa168bbb74e81451db
ducnv2509 <ducit2509@gmail.com>
2021-11-15 15:49:15 +0700

    m

--- ManagerShop/src/com/raven/form/demoCom.java
+++ ManagerShop/src/com/raven/form/demoCom.java
@@ -4,7 +4,14 @@
  * and open the template in the editor.
  */
 package com.raven.form;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.ImageIcon;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,11 +19,58 @@ import javax.swing.ImageIcon;
  */
 public class demoCom extends javax.swing.JFrame {
 
+    Connection con;
+
     /**
      * Creates new form demoCom
      */
     public demoCom() {
         initComponents();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        con = ketnoi("dbDemoInsert");
+        if (con != null) {
+            System.out.println("OK");
+        }
+    }
+
+    public static Connection ketnoi(String database) {
+        try {
+            String user = "sa";
+            String pass = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=" + database;
+            Connection cn = DriverManager.getConnection(url, user, pass);
+            return cn;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void save(demoCoTheXoa dm) {
+        String sql = "INSERT INTO dbo.[USER]\n"
+                + "(name,address,phoneNumber)\n"
+                + "VALUES\n"
+                + "(?,?,?)";
+        try {
+            try (PreparedStatement stmt = con.prepareStatement(sql)) {
+                stmt.setString(1, dm.getName());
+                stmt.setString(2, dm.getAddress());
+                stmt.setString(3, dm.getPhoneNumber());
+                stmt.executeUpdate();
+            }
+            String sql2 = "INSERT INTO dbo.Account\n"
+                    + "(idUser,Username,password)\n"
+                    + "VALUES(?, ?,?)";
+            try (PreparedStatement stmt = con.prepareStatement(sql2)) {
+                stmt.setInt(1, dm.getIdUser());
+                stmt.setString(2, dm.getUsername());
+                stmt.setString(3, dm.getPaswword());
+                stmt.executeUpdate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -29,32 +83,28 @@ public class demoCom extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tableColumn1 = new com.raven.suportSwing.TableColumn();
         scrollBar1 = new com.raven.suportSwing.ScrollBar();
         scrollBarCustom1 = new com.raven.suportSwing.ScrollBarCustom();
-        lblBell = new javax.swing.JLabel();
+        txtName = new javax.swing.JTextField();
+        txtPhone = new javax.swing.JTextField();
+        txtUsername = new javax.swing.JTextField();
+        txtPassword = new javax.swing.JTextField();
+        txtAddress = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jScrollPane1.setVerticalScrollBar(scrollBar1);
-
-        tableColumn1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "ID NCC", "Tên NCC", "Địa Chỉ", "SĐT"
+        txtName.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNameActionPerformed(evt);
             }
-        ));
-        jScrollPane1.setViewportView(tableColumn1);
+        });
 
-        lblBell.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/Accept.png"))); // NOI18N
-        lblBell.setText("jLabel1");
+        txtPhone.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPhoneActionPerformed(evt);
+            }
+        });
 
         jButton1.setText("jButton1");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -68,46 +118,101 @@ public class demoCom extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 669, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(230, 230, 230)
-                        .addComponent(scrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(150, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGap(164, 164, 164)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtName)
+                            .addComponent(txtUsername, javax.swing.GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE))
+                        .addGap(26, 26, 26)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(lblBell)
-                                .addGap(393, 393, 393))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jButton1)
-                                .addGap(253, 253, 253))))))
+                            .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtPhone, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(521, 521, 521)
+                                .addComponent(scrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(51, 51, 51)
+                                .addComponent(txtAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(426, 426, 426)
+                        .addComponent(jButton1)))
+                .addContainerGap(145, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(83, 83, 83)
+                        .addGap(57, 57, 57)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(scrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(60, 60, 60)
-                .addComponent(lblBell)
-                .addGap(33, 33, 33)
+                        .addGap(118, 118, 118)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(22, 22, 22)
                 .addComponent(jButton1)
-                .addContainerGap(162, Short.MAX_VALUE))
+                .addContainerGap(269, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void txtNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNameActionPerformed
+
+    private void txtPhoneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPhoneActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtPhoneActionPerformed
+
+    public List<demoCoTheXoa> getAll() {
+        List<demoCoTheXoa> list = new ArrayList<>();
+        try {
+            con = ketnoi("dbDemoInsert");
+            String sql = "SELECT * FROM dbo.Account JOIN dbo.[USER] ON [USER].idUser = Account.idUser";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                demoCoTheXoa sv = new demoCoTheXoa(rs.getString("name"), rs.getString("address"),
+                        rs.getString("phoneNumber"), rs.getString("username"), rs.getString("password"),
+                        rs.getInt("idUser"), rs.getInt("idAccount"));
+                list.add(sv);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    List<demoCoTheXoa> list = new ArrayList<>();
+
+    public boolean check(int ac) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getIdUser() == ac) {
+                return true;
+            }
+        }
+        return false;
+    }
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        lblBell.hide();
+        demoCoTheXoa dm = new demoCoTheXoa();
+        dm.setName(txtName.getText());
+        dm.setPhoneNumber(txtPhone.getText());
+        dm.setAddress(txtAddress.getText());
+        dm.setUsername(txtUsername.getText());
+        dm.setPaswword(txtPassword.getText());
+        dm.setIdUser(WIDTH);
+        save(dm);
+        JOptionPane.showMessageDialog(this, "OK");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -147,10 +252,12 @@ public class demoCom extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JLabel lblBell;
     private com.raven.suportSwing.ScrollBar scrollBar1;
     private com.raven.suportSwing.ScrollBarCustom scrollBarCustom1;
-    private com.raven.suportSwing.TableColumn tableColumn1;
+    private javax.swing.JTextField txtAddress;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JTextField txtPassword;
+    private javax.swing.JTextField txtPhone;
+    private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
 }
