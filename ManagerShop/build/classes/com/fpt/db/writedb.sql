d3fc04f12a1ea1739d91890b9b9debfcfd6203bf
vuNhatMinh <55698410+SpringMike@users.noreply.github.com>
2021-11-18 23:06:43 +0700

    final 11:06 18/11

--- ManagerShop/build/classes/com/fpt/db/writedb.sql
+++ ManagerShop/build/classes/com/fpt/db/writedb.sql
@@ -1,2 +1,5 @@
 ﻿ALTER TABLE dbo.Material ALTER COLUMN valueMaterial NVARCHAR(50) NOT NULL
 ALTER TABLE dbo.Color ALTER COLUMN valueColor NVARCHAR(50) NOT NULL
+
+insert into Material values (N'vải')
+select * from material
