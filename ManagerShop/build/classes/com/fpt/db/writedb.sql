795f96998f84422926153fa167ef3d1cd95ecfe0
ducnv2509 <ducit2509@gmail.com>
2021-11-19 22:18:09 +0700

    done fix

--- ManagerShop/build/classes/com/fpt/db/writedb.sql
+++ ManagerShop/build/classes/com/fpt/db/writedb.sql
@@ -5,9 +5,10 @@ ALTER TABLE dbo.Products ADD statusDelete BIT DEFAULT 1
 -- xoá
 ALTER TABLE dbo.Products DROP CONSTRAINT FK__Products__idSupp__44FF419A
 ALTER TABLE dbo.Products DROP COLUMN idSupplier
-
 ALTER TABLE dbo.List ADD status BIT DEFAULT 1
 -----------------------------
 SELECT * FROM dbo.List
 
 UPDATE dbo.List SET status = 0 WHERE idList NOT IN (SELECT idList FROM dbo.Products) AND List.idList = ?
+
+-- vũ ngáo.....(code siêu bẩn)
